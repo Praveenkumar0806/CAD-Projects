96a765feda4ff4dc97912b54f2a7a7f6c0cc2898
Praveenkumar <swagyboypraveen0806@gmail.com>
2026-05-18 23:06:51 +0530

    Delete ProjectTrain directory

--- ProjectTrain/src/com/Project/praveen/traintrace/MainTrain.java
+++
@@ -1,9 +0,0 @@
-package com.zsgs.praveen.traintrace;
-
-import com.zsgs.praveen.traintrace.features.login.LoginView;
-
-public class MainTrain {
-    public static void main(String[] args) {
-        new LoginView().init();
-    }
-}
