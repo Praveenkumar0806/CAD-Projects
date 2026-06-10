e1ade519b6e0fdfeb2462f8d58fb457f7320857c
Praveenkumar <swagyboypraveen0806@gmail.com>
2026-06-10 12:22:01 +0530

    Delete WorkManagement directory

--- WorkManagement/src/com/praveen/project/WMS/WorkManageMain.java
+++
@@ -1,9 +0,0 @@
-package com.praveen.project.WMS;
-
-import com.praveen.project.WMS.features.Login.LoginView;
-
-public class WorkManageMain {
-    public static void main(String[] args) {
-        new LoginView().init();
-    }
-}
--- WorkManagement/src/com/praveen/project/WMS/util/AppScanner.java
+++
@@ -1,9 +0,0 @@
-package com.praveen.project.WMS.util;
-
-import java.util.Scanner;
-
-public class AppScanner {
-    private static final Scanner sc = new Scanner(System.in);
-    private AppScanner() {}
-    public static Scanner get() { return sc; }
-}
